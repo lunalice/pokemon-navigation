83921cc6920bfa41e10baa15ca816f6900a0d343
lunalice <yamashita0120@gmail.com>
2022-07-31 23:16:19 +0900

    opg setting

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,6 +2,11 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { GoogleAnalytics } from '../lib/gtag'
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+const baseUrl: string | undefined = {
+  production: "https://pokemon-navigation.vercel.app",
+  development: "http://localhost:3001",
+  test: "http://localhost:3001",
+}[process.env.NODE_ENV];
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: any) {
@@ -10,10 +15,17 @@ class MyDocument extends Document {
   }
 
   render() {
+
     return (
       <Html lang="ja">
         <Head>
           <link rel="icon" href="/favicon.ico" />
+          <meta property="og:url" content={baseUrl} />
+          <meta property="og:title" content="Pokemon Navigation" />
+          <meta property="og:site_name" content="Pokemon Navigation" />
+          <meta property="og:description" content="This site is a site where you can search by Pokemon name." />
+          <meta property="og:type" content="website" />
+          <meta property="og:image" content={`${baseUrl}/ogp.png`} />
           <link href="https://unpkg.com/nes.css@2.3.0/css/nes.min.css" rel="stylesheet" />
           <GoogleAnalytics />
         </Head>
